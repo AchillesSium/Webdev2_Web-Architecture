5d173ab4a6260b2b928011abcb5fb43239b7e29c
Mohammad Aqib Hassan <mohammad.hassan@tuni.fi>
2021-04-27 00:25:38 +0300

    docker compose file changed but mongo is not working accordingly

--- backend/server-b/index.js
+++ backend/server-b/index.js
@@ -1,8 +1,8 @@
 var sendTask = require('./rabbit-utils/sendTask');
 var receiveTask = require('./rabbit-utils/receiveTask');
 
-var orderQueueName = "order-queue";
-var completeQueueName = "order-complete-queue";
+var orderQueueName = "Order_Queue";
+var completeQueueName = "Completed_Order_Queue";
 var rabbitMQHost = "rapid-runner-rabbit:5672";
 
 console.log("Server B going to start");
